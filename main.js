93d22cff319c1b82a1b6891c1e0d162ee00d54c5
navya687 <pp4871372@gmail.com>
2025-06-29 16:14:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// Load aarti lyrics
+const lyrics = `
+    Sukhकर्ता Karata Kartāra |
+    Vighna Vināyaka Nandana ||
+    Surabhi Pada Vāranaāsan |
+    Gaṇapati Gaura Suta Vandan ||
+    ...
+`;
+document.getElementById("lyrics").innerHTML = lyrics;
+
+// Load aarti video
+const videoUrl = "https://www.youtube.com/embed/VIDEO_ID";
+document.getElementById("video-player").src = videoUrl;
+
+
